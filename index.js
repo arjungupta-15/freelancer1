016817d63ecd276517d0ad942ce997843baac50b
arjungupta_15 <ag.arjungupta15@gmai.com>
2025-01-01 14:09:41 +0530

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: 'http://localhost:5177',
+    origin: 'https://warm-mochi-f75130.netlify.app/',
     credentials: true
 }
 app.use(cors(corsOptions));
